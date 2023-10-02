b4a11c76259c1fafc79806c97fc8f73a0c94433d
alertas3 <justinaszailskas@gmail.com>
2023-10-02 22:44:12 +0300

    Elementu rikiavimas didejimo tvarka

---
+++ ElementuRikiavimas/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int masyvas [100];
+    int n, temp;
+    cout <<"Iveskite masyvo elementu skaiciu \n";
+    cin >> n;
+    cout <<"Iveskite masyvo elementus: \n";
+    for (int i = 0; i < n; i++) {
+        cout<<i+1<<" -aji masyvo elementa ";
+        cin>>masyvas[i];
+        cout<<"Ivestas "<<i+1<<" elementas "<<masyvas[i]<<endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout<<"Print i: "<<i<<endl;
+        for (int j = i + 1; j < n; j++) {
+            cout<<"Print j: "<<j<<endl;
+            if( masyvas[i] > masyvas[j]) {
+                temp = masyvas[i];
+                masyvas[i] = masyvas[j];
+                masyvas[j] = temp;
+            }
+        }
+    }
+    cout<<"Isrikiuoti elementai: "<<endl;
+    for ( int i = 0; i < n; i++) {
+        cout<<masyvas[i] <<"\t";
+    }
+
+    return 0;
+}
